d5f6d6c836996db8a2a6fa4d63f5e8fe34635604
Juani <nacho.mone@gmail.com>
2019-05-28 19:47:35 -0300

    28-05-2019-00

---
+++ Ejercicios/Clase5/App1/index.php
@@ -0,0 +1,11 @@
+<?php
+    /*Consultas SQL
+        INSERT INTO localidad(descripcion) VALUES ("QUILMES"),("LANUS"),("CABA");
+        INSERT INTO alumnos (nombre,legajo,idLocalidad) VALUES ('BRINO', 123456789, 1), ('TOTI', 987654321, 1),('SANTI', 546789123, 2),('MER', 789123456, 2),('MECHA', 741852963, 3),('ELI', 963258741, 3);
+        UPDATE `alumnos` SET nombre="JOSE" WHERE id=3;
+        UPDATE `alumnos` SET nombre="MARIA" WHERE nombre="JOSE";
+    
+    */
+
+
+?>
