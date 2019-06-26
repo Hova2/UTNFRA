ff5542019f6cab757efb65433b038e408a2d8bd0
Juan <nacho.mone@gmail.com>
2019-06-25 21:59:04 -0300

    2019-06-25-02

--- Parciales/primero/clases/controlador/ListadoDeImagenes.php
+++ Parciales/primero/clases/controlador/ListadoDeImagenes.php
@@ -2,10 +2,11 @@
     if(isset($_GET[1])){
         if (strcmp($_GET[1], 'borradas')==0){
             $directorio = __DIR__ . '/../../bds/BackUpFotos/';
-            $borradas  = scandir($directorio);    
+            $borradas  = scandir($directorio);
 
             if(sizeof($borradas)>0){
-                echo "<table>";
+                echo "<table border=1>";
+                echo "<caption><h2>Fotos Borradas</h2></caption>";
                 echo "<tr>";
                 echo "<th>Foto</th>";
                 echo "<th>Nombre</th>";
@@ -13,7 +14,9 @@
                 for($i=2 ; $i < sizeof($borradas) ; $i++){
                     echo "<tr>";
                     echo "<td>";
-                    echo "<img src=" . 'C:\TEST\UTNFRA\Parciales\primero\clases\bds\BackUpFotos\' . $borradas[$i] . ">";
+                    echo "<img src=http://localhost/UTNFRA/Parciales/Primero/bds/BackUpFotos/";
+                    echo $borradas[$i];
+                    echo '>';
                     echo "</td>";
                     echo "<td>";
                     echo $borradas[$i];
@@ -21,14 +24,54 @@
                     echo "</tr>";
                 }
                 echo "</table>";
+            }
         }else{
-            $fotos1  = scandir(__DIR__ . '/../../bds/ImagenesDeLaVenta/');    
-            $fotos2  = scandir(__DIR__ . '/../../bds/ImagenesDePizzas/');    
-            var_dump($fotos1);
-            var_dump($fotos2);
+            $fotos1=scandir(__DIR__ . '/../../bds/ImagenesDeLaVenta/');
+            if(sizeof($fotos1)>0){
+                echo "<table border=1>";
+                echo "<caption><h2>Fotos de las Ventas</h2></caption>";
+                echo "<tr>";
+                echo "<th>Foto</th>";
+                echo "<th>Nombre</th>";
+                echo "</tr>";
+                for($i=2 ; $i < sizeof($fotos1) ; $i++){
+                    echo "<tr>";
+                    echo "<td>";
+                    echo "<img src=http://localhost/UTNFRA/Parciales/Primero/bds/ImagenesDeLaVenta/";
+                    echo $fotos1[$i];
+                    echo '>';
+                    echo "</td>";
+                    echo "<td>";
+                    echo $fotos1[$i];
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            $fotos2  = scandir(__DIR__ . '/../../bds/ImagenesDePizzas/'); 
+            if(sizeof($fotos2)>0){
+                echo "<table border=1>";
+                echo "<caption><h2>Fotos de las Pizzas</h2></caption>";
+                echo "<tr>";
+                echo "<th>Foto</th>";
+                echo "<th>Nombre</th>";
+                echo "</tr>";
+                for($i=2 ; $i < sizeof($fotos2) ; $i++){
+                    echo "<tr>";
+                    echo "<td>";
+                    echo "<img src=http://localhost/UTNFRA/Parciales/Primero/bds/ImagenesDePizzas/";
+                    echo $fotos2[$i];
+                    echo '>';
+                    echo "</td>";
+                    echo "<td>";
+                    echo $fotos2[$i];
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
         }
     }else{
         echo "<h2>Ingrese parametros</h2>";
     }
-}
 ?>
