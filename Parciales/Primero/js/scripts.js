19cd6b879e28ca26f103c39fa0fcdbc941b3a45b
Juani <nacho.mone@gmail.com>
2019-07-09 19:43:53 -0300

    2019-07-09-01

--- Parciales/Primero/js/scripts.js
+++ Parciales/Primero/js/scripts.js
@@ -2,15 +2,16 @@ window.addEventListener('load', asignarManejadores, false);
 window.addEventListener('load', traerPersonajes, false);
 
 function asignarManejadores(){
-    document.getElementById('btnSetPersonaje').addEventListener('click',formulario,false);  
+    document.getElementById('btnSetPersonaje').addEventListener('click', formulario, false);  
 }
 
 function crearTabla(arregloDatos){
-    var tabla           = document.createElement("table");
-    var contenedor      = document.getElementById('contenedor');
-    var indices         = Object.keys(arregloDatos[0]);
-    var tr              = tabla.insertRow(-1);
-        tabla.className = "tablas";
+    var tabla      = document.createElement("table");
+    var contenedor = document.getElementById('contenedor');
+    var indices    = Object.keys(arregloDatos[0]);
+    var tr         = tabla.insertRow(-1);
+        
+    tabla.className = "tablas";
 
     indices.forEach(dato => {
         var th           = document.createElement("th");
@@ -20,6 +21,7 @@ function crearTabla(arregloDatos){
 
     arregloDatos.forEach(dato => {
         var tr = tabla.insertRow(-1);
+        tr.addEventListener('click', formulario, false);
         Object.values(dato).forEach(dato2 => {
             var td           = tr.insertCell(-1);
                 td.innerHTML = dato2;
@@ -72,7 +74,7 @@ function formulario(){
     var lannister = document.createElement('input');
     var targaryen = document.createElement('input');
     var trai      = document.createElement('input');
-    var enviar    = document.createElement('input');
+    var cerrar    = document.createElement('input');
     
     var lNombre    = document.createElement('label');
     var lApellido  = document.createElement('label');
@@ -82,10 +84,9 @@ function formulario(){
     var lTargaryen = document.createElement('label');
     var lTrai      = document.createElement('label');
     
-    formulario.className = 'frmGenerico';
-    enviar.className     = 'boton';
-    
-    enviar.addEventListener("click", altaPersonaje , false);
+    cerrar.className = 'boton tamFuenteBoton';
+
+    cerrar.addEventListener("click", cerrarFormulario , false);
     
     formulario.id = 'formulario';
     nombre.id     = 'nombre';
@@ -95,6 +96,7 @@ function formulario(){
     lannister.id  = "lannister";
     targaryen.id  = "targaryen";
     trai.id       = 'traidor';
+    cerrar.id     = "cerrar";
 
     nombre.type    = "text";
     apellido.type  = "text";
@@ -103,8 +105,8 @@ function formulario(){
     lannister.type = "radio";
     targaryen.type = "radio";
     trai.type      = "checkbox";
-    enviar.type    = "submit";
-    
+    cerrar.type    = "button";
+
     nombre.name    = "nombre";
     apellido.name  = "apellido";
     edad.name      = "edad";
@@ -112,12 +114,12 @@ function formulario(){
     lannister.name = "casa";
     targaryen.name = "casa";
     trai.name      = "traidor";
-    enviar.name    = "enviar";
+    cerrar.name    = "cerrar";
 
     stark.value     = "Stark";
     lannister.value = "Lannister";
     targaryen.value = "Targaryen";
-    enviar.value    = "Enviar";
+    cerrar.value    = "Cerrar";
 
     lNombre.htmlFor    = "nombre";
     lApellido.htmlFor  = "apellido";
@@ -149,8 +151,58 @@ function formulario(){
     formulario.appendChild(targaryen);
     formulario.appendChild(lTrai);
     formulario.appendChild(trai);
-    formulario.appendChild(enviar);
     
+    if(this.nodeName.localeCompare('INPUT') === 0){
+        var enviar = document.createElement('input');
+        
+        formulario.className = 'frmGenericoA fuente tamFuente';
+
+        enviar.className = 'boton tamFuenteBoton';
+        enviar.id        = "enviar";
+        enviar.type      = "submit";
+        enviar.name      = "enviar";
+        enviar.value     = "Enviar";
+        
+        enviar.addEventListener("click", altaPersonaje , false);
+
+        formulario.appendChild(enviar);
+    }else{
+        var id                   = document.createElement('input');
+        var modificar            = document.createElement('input');
+        var eliminar             = document.createElement('input');
+            formulario.className = 'frmGenericoMB fuente tamFuente';
+        
+        id.id               = 'id';
+        id.type             = "hidden";
+        id.name             = "id";
+        modificar.className = 'boton tamFuenteBoton';
+        modificar.id        = "modificar";
+        modificar.type      = "submit";
+        modificar.name      = "modificar";
+        modificar.value     = "Modificar";
+        eliminar.className  = 'boton tamFuenteBoton';
+        eliminar.id         = "eliminar";
+        eliminar.type       = "submit";
+        eliminar.name       = "eliminar";
+        eliminar.value      = "Eliminar";
+
+                                                                                                            id.value       = this.childNodes[0].textContent;
+                                                                                                            nombre.value   = this.childNodes[1].textContent;
+                                                                                                            apellido.value = this.childNodes[2].textContent;
+                                                                                                            edad.value     = this.childNodes[3].textContent;
+                                                                                        formulario.elements['casa'].value  = this.childNodes[4].textContent;
+                                                                                                            trai.checked   = JSON.parse(this.childNodes[5].textContent);
+
+        modificar.addEventListener("click", modificarPersonaje , false);
+        eliminar.addEventListener("click", bajaPersonaje , false);
+
+        formulario.appendChild(id);
+        formulario.appendChild(modificar);
+        formulario.appendChild(eliminar);
+    }
+    
+    formulario.appendChild(cerrar);
+
     document.body.appendChild(formulario);
 }
 
@@ -161,19 +213,20 @@ function altaPersonaje(){
     var edad          = elementosForm['edad'].value;
     var casa          = elementosForm['casa'].value;
     var trai          = elementosForm['traidor'];
-    var xhr = new XMLHttpRequest();
-    var datos="";
+    var xhr           = new XMLHttpRequest();
+    var datos         = "";
     
-    datos=datos.concat("nombre=");
-    datos=datos.concat(encodeURIComponent(nombre));
-    datos=datos.concat("&apellido=");
-    datos=datos.concat(encodeURIComponent(apellido));
-    datos=datos.concat("&edad=");
-    datos=datos.concat(encodeURIComponent(edad));
-    datos=datos.concat("&casa=");
-    datos=datos.concat(encodeURIComponent(casa));
-    datos=datos.concat("&traidor=");
-    datos=datos.concat(encodeURIComponent(trai.checked));
+    datos = datos.concat("nombre=");
+    datos = datos.concat(encodeURIComponent(nombre));
+    datos = datos.concat("&apellido=");
+    datos = datos.concat(encodeURIComponent(apellido));
+    datos = datos.concat("&edad=");
+    datos = datos.concat(encodeURIComponent(edad));
+    datos = datos.concat("&casa=");
+    datos = datos.concat(encodeURIComponent(casa));
+    datos = datos.concat("&traidor=");
+    datos = datos.concat(encodeURIComponent(trai.checked));
+    
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
@@ -187,4 +240,72 @@ function altaPersonaje(){
     xhr.open("POST", "http://localhost:3000/altaPersonaje", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(datos);
+}
+
+function modificarPersonaje(){
+    var elementosForm = document.getElementById('formulario').elements;
+    var id            = elementosForm['id'].value;
+    var nombre        = elementosForm['nombre'].value;
+    var apellido      = elementosForm['apellido'].value;
+    var edad          = elementosForm['edad'].value;
+    var casa          = elementosForm['casa'].value;
+    var trai          = elementosForm['traidor'];
+    var xhr           = new XMLHttpRequest();
+    var datos         = "";
+    
+    datos = datos.concat("id=");
+    datos = datos.concat(encodeURIComponent(id));
+    datos = datos.concat("&nombre=");
+    datos = datos.concat(encodeURIComponent(nombre));
+    datos = datos.concat("&apellido=");
+    datos = datos.concat(encodeURIComponent(apellido));
+    datos = datos.concat("&edad=");
+    datos = datos.concat(encodeURIComponent(edad));
+    datos = datos.concat("&casa=");
+    datos = datos.concat(encodeURIComponent(casa));
+    datos = datos.concat("&traidor=");
+    datos = datos.concat(encodeURIComponent(trai.checked));
+          
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                alert(xhr.responseText);
+            }
+            else {
+                console.log('Error ' + xhr.statusText);
+            }
+        }
+    };
+    xhr.open("POST", "http://localhost:3000/modificarPersonaje", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(datos);
+}
+
+function bajaPersonaje(){
+    var elementosForm = document.getElementById('formulario').elements;
+    var id            = elementosForm['id'].value;
+    var xhr           = new XMLHttpRequest();
+    var datos         = "";
+    
+    datos = datos.concat("id=");
+    datos = datos.concat(encodeURIComponent(id));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                alert(xhr.responseText);
+            }
+            else {
+                console.log('Error ' + xhr.statusText);
+            }
+        }
+    };
+    
+    xhr.open("POST", "http://localhost:3000/bajaPersonaje", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(datos);
+}
+
+function cerrarFormulario(){
+    document.body.removeChild(document.getElementById('formulario'));
 }
