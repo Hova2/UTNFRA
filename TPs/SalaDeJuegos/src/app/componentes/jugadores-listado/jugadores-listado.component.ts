3a7a558de28499905a0eca2b1d994de86f6c8be9
Juani <nacho.mone@gmail.com>
2019-10-07 00:52:16 -0300

    2019-10-07-00

--- TPs/SalaDeJuegos/src/app/componentes/jugadores-listado/jugadores-listado.component.ts
+++ TPs/SalaDeJuegos/src/app/componentes/jugadores-listado/jugadores-listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Jugador } from 'src/app/clases/jugador';
 import { JugadoresService } from '../../servicios/jugadores.service';
 @Component({
   selector: 'app-jugadores-listado',
@@ -6,42 +7,24 @@ import { JugadoresService } from '../../servicios/jugadores.service';
   styleUrls: ['./jugadores-listado.component.css']
 })
 export class JugadoresListadoComponent implements OnInit {
+  public listadoJugadores: Array<Jugador>;
 
-  listado:any
-  miJugadoresServicio:JugadoresService
-  
-    constructor(serviceJugadores:JugadoresService) {
-      this.miJugadoresServicio = serviceJugadores;
-      
-    }
-    
+  constructor(private jugServ: JugadoresService) {}
 
+  ngOnInit() {}
 
-  ngOnInit() {
+  public traerTodos(): Array<Jugador> {
+    this.listadoJugadores = this.jugServ.traerJugadoresTodos('todos');
+    return this.listadoJugadores;
   }
 
-
-  TraerTodos(){
-    //alert("totos");
-    this.miJugadoresServicio.traertodos('jugadores/','todos').then(data=>{
-      //console.info("jugadores listado",(data));
-      this.listado= data;
-
-    })
-  }
-  TraerGanadores(){
-    this.miJugadoresServicio.traertodos('jugadores/','ganadores').then(data=>{
-      //console.info("jugadores listado",(data));
-      this.listado= data;
-
-    })
-  }
-  TraerPerdedores(){
-    this.miJugadoresServicio.traertodos('jugadores/','perdedores').then(data=>{
-      //console.info("jugadores listado",(data));
-      this.listado= data;
-
-    })
+  public traerGanadores(): Array<Jugador> {
+    this.listadoJugadores = this.jugServ.traerJugadoresTodos('ganadores');
+    return this.listadoJugadores;
   }
 
+  public traerPerdedores(): Array<Jugador> {
+    this.listadoJugadores = this.jugServ.traerJugadoresTodos('perdedores');
+    return this.listadoJugadores;
+  }
 }
--- TPs/SalaDeJuegos/src/app/servicios/juego-service.service.ts
+++ TPs/SalaDeJuegos/src/app/servicios/juego-service.service.ts
@@ -1,66 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Juego } from '../clases/juego';
-import { JuegoAdivina } from '../clases/juego-adivina';
-import { MiHttpService } from './mi-http/mi-http.service';
 
-@Injectable()
-export class JuegoServiceService {
+@Injectable({
+  providedIn: 'root'
+})
+export class JuegoService {
+  private listaJuegos: Array<Juego>;
 
-  peticion:any;
-  constructor( public miHttp: MiHttpService ) {
-    this.peticion = this.miHttp.httpGetO("http://localhost:3003");
-//    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
+  constructor() {
+    this.listaJuegos = new Array<Juego>();
   }
 
-  public listar(): Array<Juego> {
-   this.miHttp.httpGetP("https://restcountries.eu/rest/v2/all")
-    .then( data => {
-      console.log( data );
-    })
-    .catch( err => {
-      console.log( err );
-    });
-
-
-    this.peticion
-    .subscribe( data => {
-      console.log("En listar");
-      console.log( data );
-    }, err => {
-      console.info("error: " ,err );
-    })
-
-    let miArray: Array<Juego> = new Array<Juego>();
-
-    miArray.push(new JuegoAdivina("Juego 1", false));
-    miArray.push(new JuegoAdivina("Pepe", true));
-    miArray.push(new JuegoAdivina("Juego 3", false));
-    miArray.push(new JuegoAdivina("Juego 4", false));
-    miArray.push(new JuegoAdivina("Juego 5", false));
-    miArray.push(new JuegoAdivina("Juego 6", false));
-    return miArray;
+  public altaJuego(juego: Juego) {
+    this.listaJuegos.push(juego);
   }
 
-  public listarPromesa(): Promise<Array<Juego>> {
-    this.peticion
-    .subscribe( data => {
-      console.log("En listarPromesa");
-      console.log( data );
-    }, err => {
-      console.log( err );
-    })
-    let promesa: Promise<Array<Juego>> = new Promise((resolve, reject) => {
-      let miArray: Array<Juego> = new Array<Juego>();
-      miArray.push(new JuegoAdivina("JuegoPromesa 1", false,"promesa"));
-      miArray.push(new JuegoAdivina("PepePromesa", true));
-      miArray.push(new JuegoAdivina("JuegoPromesa 3", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 4", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 5", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 6", false));
-      resolve(miArray);
-    });
-
-    return promesa;
+  public traerJuegosTodos(): Array<Juego> {
+    return this.listaJuegos;
   }
-
 }
