fb8fc2da385c784d84bda90d3da5e5ed3ed7b4c5
Juani <nacho.mone@gmail.com>
2019-10-07 13:04:02 -0300

    2019-10-07-04

--- TPs/SalaDeJuegos/src/app/componentes/registro/registro.component.ts
+++ TPs/SalaDeJuegos/src/app/componentes/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Jugador } from 'src/app/clases/jugador';
+import { Sexo } from 'src/app/enums/sexo.enum';
+import { JugadoresService } from 'src/app/servicios/jugadores.service';
 //para poder hacer las validaciones
 //import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
@@ -11,14 +13,19 @@ import { Jugador } from 'src/app/clases/jugador';
 })
 export class RegistroComponent implements OnInit {
   public jugador: Jugador;
+  public email: string;
+  public cuit: string;
+  public clave: string;
+  public sexo: Sexo;
 
 
-  constructor() {}
+  constructor(private jugServ: JugadoresService) {}
 
   ngOnInit() {}
 
   public altaJugador(){
-
+    this.jugador = new Jugador(this.email, this.cuit, this.sexo, false, this.clave);
+    this.jugServ.altaJugador(this.jugador);
   }
 
 
