6162a1782b9f0db4be482b201bcd8bdba8ccb5cb
Juan <nacho.mone@gmail.com>
2019-11-19 19:19:03 -0300

    Agrego firebase"

--- TPs/TP00/src/environments/environment.prod.ts
+++ TPs/TP00/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDixKTzZThi_YBdu74Zs_jGG2Fn-TA53AQ',
+    authDomain: 'tpbd-616aa.firebaseapp.com',
+    databaseURL: 'https://tpbd-616aa.firebaseio.com',
+    projectId: 'tpbd-616aa',
+    storageBucket: 'tpbd-616aa.appspot.com',
+    messagingSenderId: '928844116589',
+    appId: '1:928844116589:web:92c9d78923271691e48564'
+  }
 };
