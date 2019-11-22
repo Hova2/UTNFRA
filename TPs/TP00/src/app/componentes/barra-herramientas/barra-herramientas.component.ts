beded9c5660f2f4037d03f7c5389c583886ab2ea
Juani <nacho.mone@gmail.com>
2019-11-22 18:17:56 -0300

    avatar

--- TPs/TP00/src/app/componentes/barra-herramientas/barra-herramientas.component.ts
+++ TPs/TP00/src/app/componentes/barra-herramientas/barra-herramientas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from 'src/app/servicios/auth.service';
+import { Observable } from 'rxjs';
+import { UsuarioI } from 'src/app/interfaces/usuario-i';
 
 @Component({
   selector: "app-barra-herramientas",
@@ -6,7 +9,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./barra-herramientas.component.scss"]
 })
 export class BarraHerramientasComponent implements OnInit {
-  constructor() {}
+  foto: string;
+  usuario: Observable<UsuarioI>;
+
+  constructor(private as: AuthService) {
+    this.usuario = as.traerUsuarioActivo();
+    this.usuario.subscribe(usuario => {
+      this.foto = usuario.foto;
+    })
+  }
 
   ngOnInit() {}
 }
