dde80535318b587332f0e553ae6d64ab34bf247c
Juani <nacho.mone@gmail.com>
2019-12-02 23:34:31 -0300

    Punto 8 listo

--- Parciales/Parcial1/src/app/service/actor.service.ts
+++ Parciales/Parcial1/src/app/service/actor.service.ts
@@ -1,22 +1,31 @@
-import { Injectable } from "@angular/core";
-import { ActorI } from "../interface/actor-i";
+import { Injectable } from '@angular/core';
+import { ActorI } from '../interface/actor-i';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ActorService {
   ultimoId: number;
   actores: Array<ActorI>;
 
   constructor() {
-    this.ultimoId = 1;
-    this.actores = new Array<ActorI>();
+    this.ultimoId = JSON.parse(localStorage.getItem('ultimoIdActor'));
+    if (!this.ultimoId) {
+      this.ultimoId = 1;
+      this.actores = new Array<ActorI>();
+      localStorage.setItem('actores', JSON.stringify(this.actores));
+      localStorage.setItem('ultimoIdActor', JSON.stringify(this.ultimoId));
+    } else {
+      this.actores = JSON.parse(localStorage.getItem('actores'));
+      this.ultimoId = JSON.parse(localStorage.getItem('ultimoIdActor'));
+    }
   }
 
   persistirActor(actor: ActorI) {
     this.actores.push(actor);
     this.ultimoId++;
-    console.table(this.actores);
+    localStorage.setItem('actores', JSON.stringify(this.actores));
+    localStorage.setItem('ultimoIdActor', JSON.stringify(this.ultimoId));
   }
 
   traerUltimoId(): number {
--- Parciales/Parcial1/src/app/service/pelicula.service.ts
+++ Parciales/Parcial1/src/app/service/pelicula.service.ts
@@ -1,60 +1,68 @@
-import { Injectable } from "@angular/core";
-import { Pelicula } from "../class/pelicula";
-import { ETipoPelicula } from "../enum/etipo-pelicula.enum";
-import { IPelicula } from "../interface/ipelicula";
+import { Injectable } from '@angular/core';
+import { ETipoPelicula } from '../enum/etipo-pelicula.enum';
+import { IPelicula } from '../interface/ipelicula';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class PeliculaService {
-  private listPelicula = Array<IPelicula>();
+  private peliculas = Array<IPelicula>();
   ultimoId: number;
 
   constructor() {
-    this.listPelicula = new Array<IPelicula>();
-    // tslint:disable-next-line: max-line-length
-    this.listPelicula.push({
-      id: 1,
-      nombre: "Joker",
-      tipo: ETipoPelicula.OTROS,
-      fechaEstreno: "04-10-2019",
-      cantPublico: 100000,
-      fotoPelicula:
-        "https://image.tmdb.org/t/p/w600_and_h900_bestv2/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
-    });
-    this.listPelicula.push({
-      id: 2,
-      nombre: "Psycho",
-      tipo: ETipoPelicula.TERROR,
-      fechaEstreno: "16-06-1960",
-      cantPublico: 50000000,
-      fotoPelicula:
-        "https://image.tmdb.org/t/p/w600_and_h900_bestv2/81d8oyEFgj7FlxJqSDXWr8JH8kV.jpg"
-    });
-    this.listPelicula.push({
-      id: 3,
-      nombre: "Your Name.",
-      tipo: ETipoPelicula.AMOR,
-      fechaEstreno: "03-07-2016",
-      cantPublico: 10000,
-      fotoPelicula:
-        "https://image.tmdb.org/t/p/w600_and_h900_bestv2/xq1Ugd62d23K2knRUx6xxuALTZB.jpg"
-    });
-    this.ultimoId = 4;
+    this.ultimoId = JSON.parse(localStorage.getItem('ultimoIdPelicula'));
+    if (!this.ultimoId) {
+      this.ultimoId = 4;
+      this.peliculas = new Array<IPelicula>();
+      // tslint:disable-next-line: max-line-length
+      this.peliculas.push({
+        id: 1,
+        nombre: 'Joker',
+        tipo: ETipoPelicula.OTROS,
+        fechaEstreno: '04-10-2019',
+        cantPublico: 100000,
+        fotoPelicula:
+          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg'
+      });
+      this.peliculas.push({
+        id: 2,
+        nombre: 'Psycho',
+        tipo: ETipoPelicula.TERROR,
+        fechaEstreno: '16-06-1960',
+        cantPublico: 50000000,
+        fotoPelicula:
+          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/81d8oyEFgj7FlxJqSDXWr8JH8kV.jpg'
+      });
+      this.peliculas.push({
+        id: 3,
+        nombre: 'Your Name.',
+        tipo: ETipoPelicula.AMOR,
+        fechaEstreno: '03-07-2016',
+        cantPublico: 10000,
+        fotoPelicula:
+          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/xq1Ugd62d23K2knRUx6xxuALTZB.jpg'
+      });
+      localStorage.setItem('peliculas', JSON.stringify(this.peliculas));
+      localStorage.setItem('ultimoIdPelicula',JSON.stringify(this.ultimoId));
+    }else{
+      this.peliculas = JSON.parse(localStorage.getItem('peliculas'));
+      this.ultimoId = JSON.parse(localStorage.getItem('ultimoIdPelicula'));
+    }
   }
 
   public traerListado(): Array<IPelicula> {
-    return this.listPelicula;
+    return this.peliculas;
   }
 
   public borrarPelicula(pelicula: IPelicula) {
-    const indice = this.listPelicula.indexOf(pelicula);
-    this.listPelicula.splice(indice, 1);
+    const indice = this.peliculas.indexOf(pelicula);
+    this.peliculas.splice(indice, 1);
+    localStorage.setItem('peliculas', JSON.stringify(this.peliculas));
   }
 
   public buscarPelicula(nombre: string): IPelicula {
     let pelicula: IPelicula;
-    this.listPelicula.forEach(elemento => {
+    this.peliculas.forEach(elemento => {
       if (elemento.nombre === nombre) {
         pelicula = elemento;
       }
@@ -64,11 +72,13 @@ export class PeliculaService {
   }
 
   persistirPelicula(pelicula: IPelicula) {
-    this.listPelicula.push(pelicula);
+    this.peliculas.push(pelicula);
     this.ultimoId++;
+    localStorage.setItem('peliculas', JSON.stringify(this.peliculas));
+    localStorage.setItem('ultimoIdPelicula',JSON.stringify(this.ultimoId));
   }
 
-  traerUltimoId(): number{
+  traerUltimoId(): number {
     return this.ultimoId;
   }
 }
