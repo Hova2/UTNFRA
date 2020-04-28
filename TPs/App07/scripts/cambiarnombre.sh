4ded482278badba7c3daa5a2355e5c162d923bd7
Juani <nacho.mone@gmail.com>
2020-04-27 22:50:28 -0300

    2020042703

---
+++ TPs/App07/scripts/cambiarnombre.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mv /home/juani/Git/UTNFRA/TPs/App07/platforms/android/app/build/outputs/apk/debug/app-debug.apk /home/juani/Git/UTNFRA/TPs/App07/platforms/android/app/build/outputs/apk/debug/app-debug-07.apk
---
+++ TPs/App08/scripts/cambiarnombre.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mv /home/juani/Git/UTNFRA/TPs/App08/platforms/android/app/build/outputs/apk/debug/app-debug.apk /home/juani/Git/UTNFRA/TPs/App08/platforms/android/app/build/outputs/apk/debug/app-debug-08.apk
