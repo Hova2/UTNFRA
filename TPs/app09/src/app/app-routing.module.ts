687f076d7f153c5230637dbda1004829ca2ba585
Juani <nacho.mone@gmail.com>
2020-05-21 14:32:47 -0300

    2020052104

--- TPs/app09/src/app/app-routing.module.ts
+++ TPs/app09/src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
