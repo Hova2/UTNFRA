59fed9611edeb5e73fbb8dabc0776186d8dd49a8
Juani <nacho.mone@gmail.com>
2020-05-22 12:32:36 -0300

    2020052200

--- TPs/app06/src/app/pages/login/login.page.ts
+++ TPs/app06/src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public formulario: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private as: AuthService) {
+    this.formulario = new FormGroup({
+      usuario: new FormControl(null, [Validators.required]),
+      clave: new FormControl(null, [Validators.required]),
+    });
   }
 
+  ngOnInit() {}
+
+  public completarUsuario(usuario: string) {
+    switch (usuario) {
+      case 'admin':
+        this.formulario.controls.usuario.setValue('admin@admin.com');
+        this.formulario.controls.clave.setValue('11111111');
+        break;
+      case 'invitado':
+        this.formulario.controls.usuario.setValue('invitado@invitado.com');
+        this.formulario.controls.clave.setValue('22222222');
+        break;
+      case 'usuario':
+        this.formulario.controls.usuario.setValue('usuario@usuario.com');
+        this.formulario.controls.clave.setValue('33333333');
+        break;
+      case 'anonimo':
+        this.formulario.controls.usuario.setValue('anonimo@anonimo.com');
+        this.formulario.controls.clave.setValue('44444444');
+        break;
+      case 'tester':
+        this.formulario.controls.usuario.setValue('tester@tester.com');
+        this.formulario.controls.clave.setValue('55555555');
+        break;
+    }
+  }
 }
